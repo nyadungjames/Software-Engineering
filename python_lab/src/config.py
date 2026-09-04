f8080ae328b2afcd3c66617c5272397528711b14
nyadungjames <nyadungjames@gmail.com>
2026-09-04 14:29:49 +0300

    Week 1 python lab

---
+++ python_lab/src/config.py
@@ -0,0 +1,3 @@
+APP_NAME = "Python Lab"
+VERSION = "1.0.0"
+DEBUG = True
---
+++ python_lab/src/main.py
@@ -0,0 +1,8 @@
+from utils import square, is_even, celsius_to_fahrenheit
+number  = int(input("Enter a number:"))
+print("Square:", square(number))
+if is_even(number):
+    print("The number is even.")
+else:
+    print("The number is odd.")
+print("Fahrenheit:", celsius_to_fahrenheit(number))
